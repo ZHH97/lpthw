b7ea8a46db6629ac4db15e0ff73dfb1182e58a31
ZHH97 <ryanzhh1997@gmail.com>
2026-05-03 21:45:51 +0800

    今日练习

--- ex23.py
+++ ex23.py
@@ -1,4 +1,30 @@
+# import sys # 导入sys模块
+# script, encoding, error = sys.argv
+
+
+# def main(language_file, encoding, errors):
+#     line = language_file.readline()
+
+#     if line:
+#         print_line(line, encoding, errors)
+#         return main(language_file, encoding, errors)
+    
+
+# def print_line(line, encoding, errors):
+#     next_lang = line.strip()
+#     raw_bytes = next_lang.encode(encoding, errors=errors)
+#     cooked_string = raw_bytes.decode(encoding, errors=errors)
+
+#     print(raw_bytes, "<===>", cooked_string)
+
+
+# languages = open("languages.txt", encoding="utf-8")
+
+# main(languages, encoding, error)
+
+
 import sys
+
 script, encoding, error = sys.argv
 
 
@@ -11,13 +37,12 @@ def main(language_file, encoding, errors):
     
 
 def print_line(line, encoding, errors):
-    next_lang = line.strip()
-    raw_bytes = next_lang.encode(encoding, errors=errors)
+    raw_bytes = line.strip()
+
     cooked_string = raw_bytes.decode(encoding, errors=errors)
 
     print(raw_bytes, "<===>", cooked_string)
 
-
-languages = open("languages.txt", encoding="utf-8")
+languages = open("languages.txt", "rb")
 
 main(languages, encoding, error)
