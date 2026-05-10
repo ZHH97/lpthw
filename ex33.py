b1446a88a1183715cf7283e05aac395cf5e8b9ff
ZHH97 <ryanzhh1997@gmail.com>
2026-05-10 20:40:07 +0800

    今日练习

--- ex33.py
+++ ex33.py
@@ -1,17 +1,50 @@
-i = 0
-numbers = []
+# i = 0
+# numbers = []
 
-while i < 6:
-    print(f"At the top i is {i}")
-    numbers.append(i)
+# while i < 6:
+#     print(f"At the top i is {i}")
+#     numbers.append(i)
 
-    i = i + 1
-    print("Numbers now: ", numbers)
-    print(f"At the bottom i is {i}")
+#     i = i + 1
+#     print("Numbers now: ", numbers)
+#     print(f"At the bottom i is {i}")
 
 
-print("The numbers: ")
+# print("The numbers: ")
 
-for num in numbers:
-    print(num)
+# for num in numbers:
+#     print(num)
 
+# def loop_number(n):
+#     numbers = []
+#     i = 0
+
+#     while i < n:
+#         print(f"At the top i is {i}")
+#         numbers.append(i)
+
+#         i += 1
+#         print("Numbers now: ", numbers)
+#         print(f"At the bottom i is {i}")
+
+#     print("The numbers: ")
+#     for num in numbers:
+#         print(num)
+
+# loop_number(6)
+
+def loop_number(n):
+    numbers = []
+
+    for i in range(n):
+        print(f"At the top i is {i}")
+        numbers.append(i)
+
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {i + 1}")
+
+    print("The numbers: ")
+    for num in numbers:
+        print(num)
+
+loop_number(6)
